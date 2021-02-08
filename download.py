42d0fecc15a0314d05f45285b0af1b2823ec9aab
rebinsilva <rebinsilva@gmail.com>
2021-02-08 12:57:37 +0530

    first commit

---
+++ download.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  7 20:02:18 2021
+
+@author: Rebin Silva V
+
+Helper function to download partial audio
+"""
+
+from __future__ import unicode_literals
+import youtube_dl
+
+import subprocess
+import os.path
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+ydl_opts = {
+    'format': 'bestaudio',
+    'youtube_skip_dash_manifest': True,
+#     'postprocessors': [{
+#         'key': 'FFmpegExtractAudio',
+#         'preferredcodec': 'mp3',
+#         'preferredquality': '192',
+#     }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+
+
+def convert_format(time):
+    mins, secs = divmod(time, 60)
+    mins = int(mins)
+    hrs, mins = divmod(mins, 60)
+    return f'{hrs:02}:{mins:02}:{secs:05.2f}'
+
+
+def download_audio(video_id, start_time, end_time, out_dir = './', overwrite=False):
+    """
+    Parameters
+    ----------
+    video_id : youtube video id.
+    start_time : in secs.
+    end_time : in secs.
+    overwrite : if true, output file is overwritten if exists
+
+    Returns
+    -------
+    None.
+
+    """
+    # https://unix.stackexchange.com/a/388148/378846
+    out_file_name = os.path.join(out_dir, f'{video_id}_{start_time}_{end_time}.mp4')
+    
+    if overwrite and os.path.isfile(out_file_name):
+        return
+    
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info('https://youtu.be/'+video_id, download=False)
+        
+    url = info['url']
+    start = convert_format(start_time)
+    duration = convert_format(end_time-start_time)
+    # ffmpeg -ss 00:00:30.00 -i "OUTPUT-OF-FIRST URL" -t 00:00:10.00 -c copy out.mp4
+    subprocess.run(['ffmpeg', '-hide_banner', '-loglevel', 'error', '-y', '-ss', start, '-i', url, '-ss', '0', '-t', duration, '-c', 'copy', '--', out_file_name])
+    
+    
+if __name__ == '__main__':
+    download_audio('--i-y1v8Hy8', 0, 9)
---
+++ main.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  7 20:02:18 2021
+
+@author: Rebin Silva V
+
+Main function to download dataset
+"""
+
+from download import download_audio
+import tqdm
+
+import sys
+import os
+import os.path
+import csv
+import argparse
+from concurrent import futures
+
+
+
+def download_split(split_dataset, no_workers, out_dir):   
+    with futures.ThreadPoolExecutor(max_workers=no_workers) as executor:
+        to_do_map = {}
+        for row in split_dataset:
+            video_id = row[0]
+            start_time = float(row[1].strip())
+            end_time = float(row[2].strip())
+            # print(video_id, start_time, end_time)
+            future = executor.submit(download_audio, video_id, start_time, end_time, out_dir)
+            to_do_map[future] = video_id
+            
+        done_iter = futures.as_completed(to_do_map)
+        done_iter = tqdm.tqdm(done_iter, total=len(split_dataset))
+        
+        for future in done_iter:
+            # future.result()
+            try:
+                future.result()
+            except:
+                print("Unexpected error in audio" + to_do_map[future] + " :", sys.exc_info()[0])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-j", "--no_workers", type=int, help="number of subprocesses to spawn for parallel download", default=10)
+    parser.add_argument("-o", "--out_dir", help="path to store dataset", default=".")
+    args = parser.parse_args()
+    
+    out_dir = args.out_dir
+    dataset_splits = ('eval_segments.csv', 'balanced_train_segments.csv', 'unbalanced_train_segments.csv')
+    for split in dataset_splits:
+        split_dir = os.path.join(out_dir, os.path.splitext(split)[0])
+        try:
+            os.mkdir(split_dir)
+        except FileExistsError:
+            pass
+        with open(split) as csvfile:
+            split_reader = csv.reader(csvfile)
+            # skip first three lines
+            for i in range(3):
+                next(split_reader)
+            split_dataset = tuple(split_reader)
+            
+        download_split(split_dataset, args.no_workers, split_dir)
